46d643a3423a728bfebf6c9c9cdeafc19c678bdc
Sebastain Gonzalez <sebastiangzzcor@outlook.com>
2023-04-16 22:08:47 -0600

    changes in styles

--- components/command-palette/command-palette-finder/index.tsx
+++ components/command-palette/command-palette-finder/index.tsx
@@ -9,7 +9,7 @@ export default function CommandPaletteFinder() {
       type="text"
       placeholder="What do you need?"
       onChange={commandPalette.handleCommandPaletteSearch}
-      className="w-full px-2 py-4 text-white bg-transparent border-b outline-none border-neutral-700 placeholder-neutral-500"
+      className="w-full px-2 py-4 text-lg font-medium text-white bg-transparent border-b outline-none border-neutral-700 placeholder-neutral-500"
     />
   );
 }
--- components/command-palette/index.tsx
+++ components/command-palette/index.tsx
@@ -10,7 +10,7 @@ export default function CommandPalette() {
     <>
       {commandPalette.commandPaletteOpen && (
         <Portal target={PortalTarget.MODAL}>
-          <div className="flex items-center justify-center w-screen h-screen bg-neutral-600/50 backdrop-blur-sm">
+          <div className="flex items-center justify-center w-screen h-screen">
             <div className="w-full h-[402px]">
               <div className="max-w-[640px] w-full m-auto p-2 bg-neutral-900 border border-neutral-700 rounded-lg shadow-2xl">
                 <CommandPaletteFinder />
